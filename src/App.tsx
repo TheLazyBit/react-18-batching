5971792f1b7ee0d99ded7fbe95a2a3479307d7b8
TheLazyBit <daniel.kogan@gmx.net>
2022-09-06 22:43:18 +0200

    experiment

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useReducer, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [someState, setSomeState] = useState(0);
+  const [someState2, setSomeState2] = useState(0);
+  const [state, dispatch] = useReducer((prevState: Record<string, number>, action: any) => ({
+    ...prevState,
+    value: prevState.value + 1
+  }), {value: 0});
+
+  console.log('Render', someState, someState2, state);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link" 
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => {
+        console.log('clicked');
+        setSomeState(someState + 1);
+        setSomeState2(someState2 + 1);
+        new Promise((r) => r(dispatch({})));
+        new Promise((r) => setTimeout(() => r(dispatch({})), 100));
+      }}>Click Me
+      </button>
     </div>
   );
 }
